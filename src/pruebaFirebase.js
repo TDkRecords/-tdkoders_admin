f93a84591bc0e1ecf1f33ee870f536c39a43ee03
TDkRecords <tdksongs@gmail.com>
2025-12-03 17:40:22 -0500

    first commit

---
+++ src/pruebaFirebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDgIS2_ut0ECBz5rnNlym3j2ZQ8Y5N88bA",
+    authDomain: "prueba-c5bf0.firebaseapp.com",
+    projectId: "prueba-c5bf0",
+    storageBucket: "prueba-c5bf0.firebasestorage.app",
+    messagingSenderId: "909015842646",
+    appId: "1:909015842646:web:d10a6dc7a78d9359e760f7"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
